3aaf96c88df44c59dd224f96002c6a8194ab2013
j16003 <j16003@sangi.jp>
2019-10-07 15:01:14 +0900

    16進数部分を編集できるようにした

--- table.js
+++ table.js
@@ -1,32 +1,34 @@
-        /*------------------------------------------------
-         jQuery でプログラム的にテーブルを作成する
-        --------------------------------------------------*/
-        $(document).ready(function () {
-            var r_end = 11;  // 行数
-            var c_end = 5;  // 列数 
-            memory_Area = document.getElementById('memory_area');
-            var tableJQ = $('<table id="table_id1">');
-            for (var r = 1; r <= r_end; r++) {
-                var r000 = padLeft('0000', r);
-                var trJQ_r = $('<tr></tr>').appendTo(tableJQ);
-                for (var c = 1; c <= c_end; c++) {
-                    var c000 = padLeft('0000', c);
-                    $('<td>r=' + r000 + ':c=' + c000 + '</td>').appendTo(trJQ_r);
-                }
-            }
- 
-            $(memory_Area).append(tableJQ);
-        });
+/*------------------------------------------------
+jQuery でプログラム的にテーブルを作成する
+--------------------------------------------------*/
+//TODO Tableの幅を直す
+$(document).ready(function () {
+    var r_end = 500;  // 行数
+    var c_end = 3;  // 列数 
+    memory_Area = document.getElementById('memory_area');
+    var tableJQ = $('<table id="Memorytable">');
+    for (var r = 0; r < r_end; r++) {
+        var r000 = toHex(r);
+        var trJQ_r = $('<tr></tr>').appendTo(tableJQ);
+        for (var c = 0; c < c_end; c++) {
+            var c000 = c;
+            if(c == 0){
+                $('<td>' + r000 + '</td>').appendTo(trJQ_r);
+            }else if(c == 1){
+                $('<td>' +  r + ':' + '</td>').appendTo(trJQ_r);
+            }else{
+                $('<td>#0000</td>').appendTo(trJQ_r);
+            }            
+       }
+    }
+    $(memory_Area).append(tableJQ);
+});
 
-        function padLeft(pad, str) {
-            if (typeof str === 'undefined')
-                return pad;
- 
-            return (pad + str).slice(-pad.length);
-        }
-        function padRight(pad, str) {
-            if (typeof str === 'undefined')
-                return pad;
- 
-            return (str + pad).substring(0, pad.length);
-        }
+function toHex(v) {
+    return '#' + (('0000' + v.toString(16).toUpperCase()).substr(-4));
+}
+
+function memoryHexSet(addless,value){
+    let table = document.getElementById('Memorytable');
+    table.rows[ addless ].cells[ 2 ].firstChild.data = toHex(value);
+}
