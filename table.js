c09d0c272d4817300c3f08f0ce885926406b2aab
j16003 <j16003@sangi.jp>
2019-10-09 12:34:06 +0900

    フラグとスタックの編集ができるようにした

--- table.js
+++ table.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
     var r_end = 500;  // 行数
     var c_end = 6;  // 列数 
     memory_Area = document.getElementById('memory_area');
+    stack_Area = document.getElementById('stack_area');
     var tableJQ = $('<table id="Memorytable">');
     for (var r = 0; r < r_end; r++) {
         var r000 = toHex(r);
@@ -31,6 +32,27 @@ $(document).ready(function () {
        }
     }
     $(memory_Area).append(tableJQ);
+    r_end = 200;
+    tableJQ = $('<table id="Stacktable">');
+    for (var r = 0; r < (r_end + 1); r++) {
+        var r000 = toHex(65537 - ((r_end + 2) - r));
+        var trJQ_r = $('<tr></tr>').appendTo(tableJQ);
+        for (var c = 0; c < c_end; c++) {
+            var c000 = c;
+            if(c == 0){
+                $('<td>' + r000 + '</td>').appendTo(trJQ_r);
+            }else if(c == 1){
+                $('<td>#0000</td>').appendTo(trJQ_r);
+            }else if(c == 2){
+                $('<td>0</td>').appendTo(trJQ_r);
+            }else if(c == 3){
+                $('<td>0</td>').appendTo(trJQ_r);
+            }else if(c == 4){
+                $('<td>0000 0000 0000 0000</td>').appendTo(trJQ_r);
+            }              
+       }
+    }
+    $(stack_Area).append(tableJQ);
 });
 
 // toBin 2進数に変換する
@@ -146,6 +168,54 @@ function registerAllSet(address,value){
     registerBinSet(address,value);
 }
 
+// registerHexGet Registertableの16進数の値を取得する
+// 引数 
+// address  :  Registertableの番地
+// 戻り値
+// string   :  値
+function registerHexGet(address){
+    let table = document.getElementById('Registertable');
+    var v = table.rows[ address ].cells[ 1 ].firstChild.data;
+    v = parseInt(v.replace('#',''), 16);
+    return v
+}
+
+// registerUdecGet Registertableの符号なし10進数の値を取得する
+// 引数 
+// address  :  Registertableの番地
+// 戻り値
+// string   :  値
+function registerUdecGet(address){
+    let table = document.getElementById('Registertable');
+    var v = table.rows[ address ].cells[ 2 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
+// registerSdecGet Registertableの符号あり10進数の値を取得する
+// 引数 
+// address  :  Registertableの番地
+// 戻り値
+// string   :  値
+function registerSdecGet(address){
+    let table = document.getElementById('Registertable');
+    var v = table.rows[ address ].cells[ 3 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
+// registerBinGet Registertableの2進数の値を取得する
+// 引数 
+// address  :  Registertableの番地
+// 戻り値
+// string   :  値
+function registerBinGet(address){
+    let table = document.getElementById('Registertable');
+    var v = table.rows[ address ].cells[ 4 ].firstChild.data;
+    v = parseInt(v.replace(/ /g,''), 2);
+    return v
+}
+
 // memoryHexSet Memorytableの16進数の値を書き換える
 // 引数 
 // address  :  Memorytableの番地
@@ -194,10 +264,58 @@ function memoryAllSet(address,value){
     memoryBinSet(address,value);
 }
 
+// memoryHexGet Memorytableの16進数の値を取得する
+// 引数 
+// address  :  Memorytableの番地
+// 戻り値
+// string   :  値
+function memoryHexGet(address){
+    let table = document.getElementById('Memorytable');
+    var v = table.rows[ address ].cells[ 2 ].firstChild.data;
+    v = parseInt(v.replace('#',''), 16);
+    return v
+}
+
+// memoryUdecGet Memorytableの符号なし10進数の値を取得する
+// 引数 
+// address  :  Memorytableの番地
+// 戻り値
+// string   :  値
+function memoryUdecGet(address){
+    let table = document.getElementById('Memorytable');
+    var v = table.rows[ address ].cells[ 3 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
+// memorySdecGet Memorytableの符号あり10進数の値を取得する
+// 引数 
+// address  :  Memorytableの番地
+// 戻り値
+// string   :  値
+function memorySdecGet(address){
+    let table = document.getElementById('Memorytable');
+    var v = table.rows[ address ].cells[ 4 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
+// memoryBinGet Memorytableの2進数の値を取得する
+// 引数 
+// address  :  Memorytableの番地
+// 戻り値
+// string   :  値
+function memoryBinGet(address){
+    let table = document.getElementById('Memorytable');
+    var v = table.rows[ address ].cells[ 5 ].firstChild.data;
+    v = parseInt(v.replace(/ /g,''), 2);
+    return v
+}
+
 // toOverflowFlagSet Overflow Flagを設定する
 // 引数 
 // v        :  値
-function toOverflowFlagSet(value){
+function overflowFlagSet(value){
     let table = document.getElementById('Registertable');
     if(value == 0){
         table.rows[ 13 ].cells[ 4 ].firstChild.data = 0;
@@ -208,24 +326,20 @@ function toOverflowFlagSet(value){
     }
 }
 
-// toOverflowFlagSet Overflow Flagを設定する
-// 引数 
-// v        :  値
-function toOverflowFlagSet(value){
+// toOverflowFlagGet RegistertableのOverflow Flagの値を取得する
+// 戻り値
+// string   :  値
+function overflowFlagGet(){
     let table = document.getElementById('Registertable');
-    if(value == 0){
-        table.rows[ 13 ].cells[ 4 ].firstChild.data = 0;
-        table.rows[ 13 ].cells[ 1 ].firstChild.data = '〇';
-    }else{
-        table.rows[ 13 ].cells[ 4 ].firstChild.data = 1;
-        table.rows[ 13 ].cells[ 1 ].firstChild.data = '☆';
-    }
+    var v = table.rows[ 13 ].cells[ 4 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
 }
 
 // toSignFlagSet Sign Flagを設定する
 // 引数 
 // v        :  値
-function toSignFlagSet(value){
+function signFlagSet(value){
     let table = document.getElementById('Registertable');
     if(value == 0){
         table.rows[ 13 ].cells[ 5 ].firstChild.data = 0;
@@ -236,10 +350,20 @@ function toSignFlagSet(value){
     }
 }
 
+// toSignFlagGet RegistertableのSign Flagの値を取得する
+// 戻り値
+// string   :  値
+function signFlagGet(){
+    let table = document.getElementById('Registertable');
+    var v = table.rows[ 13 ].cells[ 5 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
 // toZeroFlagSet Zero Flagを設定する
 // 引数 
 // v        :  値
-function toZeroFlagSet(value){
+function zeroFlagSet(value){
     let table = document.getElementById('Registertable');
     if(value == 0){
         table.rows[ 13 ].cells[ 6 ].firstChild.data = 0;
@@ -248,4 +372,110 @@ function toZeroFlagSet(value){
         table.rows[ 13 ].cells[ 6 ].firstChild.data = 1;
         table.rows[ 13 ].cells[ 3 ].firstChild.data = 'Zero';
     }
+}
+
+// toZeroFlagGet RegistertableのZero Flagの値を取得する
+// 戻り値
+// string   :  値
+function zeroFlagGet(){
+    let table = document.getElementById('Registertable');
+    var v = table.rows[ 13 ].cells[ 6 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
+// stackHexSet Stacktableの16進数の値を書き換える
+// 引数 
+// address  :  Stacktableの番地
+// value    :  値
+function stackHexSet(address,value){
+    let table = document.getElementById('Stacktable');
+    table.rows[ address ].cells[ 2 ].firstChild.data = toHex(value);
+}
+
+// stackUdecSet Stacktableの符号なし10進数の値を書き換える
+// 引数 
+// address  :  Stacktableの番地
+// value    :  値
+function stackHexSet(address,value){
+    let table = document.getElementById('Stacktable');
+    table.rows[ address ].cells[ 3 ].firstChild.data = toHex(value);
+}
+
+// stackSdecSet Stacktableの符号あり10進数の値を書き換える
+// 引数 
+// address  :  Stacktableの番地
+// value    :  値
+function stackHexSet(address,value){
+    let table = document.getElementById('Stacktable');
+    table.rows[ address ].cells[ 4 ].firstChild.data = toHex(value);
+}
+
+// stackSdecSet Stacktableの16進数の値を書き換える
+// 引数 
+// address  :  Stacktableの番地
+// value    :  値
+function stackHexSet(address,value){
+    let table = document.getElementById('Stacktable');
+    table.rows[ address ].cells[ 5 ].firstChild.data = toHex(value);
+}
+
+// stackAllSet Stacktableの2進数、符号なし10進数、符号あり10進数、16進数の値を書き換える
+// 引数 
+// address  :  Memorytableの番地
+// value    :  値
+function stackAllSet(address,value){
+    let table = document.getElementById('Stacktable');
+    memoryHexSet(address,value);
+    memoryUdecSet(address,value);
+    memorySdecSet(address,value);
+    memoryBinSet(address,value);
+}
+
+// stackHexGet Stacktableの16進数の値を取得する
+// 引数 
+// address  :  Stacktableの番地
+// 戻り値
+// string   :  値
+function stackHexGet(address){
+    let table = document.getElementById('Stacktable');
+    var v = table.rows[ address ].cells[ 2 ].firstChild.data;
+    v = parseInt(v.replace('#',''), 16);
+    return v
+}
+
+// stackUdecGet Stacktableの符号なし10進数の値を取得する
+// 引数 
+// address  :  Stacktableの番地
+// 戻り値
+// string   :  値
+function stackUdecGet(address){
+    let table = document.getElementById('Stacktable');
+    var v = table.rows[ address ].cells[ 3 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
+// stackUdecGet Stacktableの符号あり10進数の値を取得する
+// 引数 
+// address  :  Stacktableの番地
+// 戻り値
+// string   :  値
+function stackUdecGet(address){
+    let table = document.getElementById('Stacktable');
+    var v = table.rows[ address ].cells[ 4 ].firstChild.data;
+    v = parseInt(v, 10);
+    return v
+}
+
+// stackBinGet stacktableの2進数の値を取得する
+// 引数 
+// address  :  Stacktableの番地
+// 戻り値
+// string   :  値
+function stackBinGet(address){
+    let table = document.getElementById('Stacktable');
+    var v = table.rows[ address ].cells[ 5 ].firstChild.data;
+    v = parseInt(v.replace(/ /g,''), 2);
+    return v
 }
