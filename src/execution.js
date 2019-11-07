f5711eeb57c84aa46928c74b94a4c20efee46445
j16003 <j16003@sangi.jp>
2019-11-07 11:57:22 +0900

    ajax

--- src/execution.js
+++ src/execution.js
@@ -11,7 +11,7 @@ function onLoadExe() {
         //メモリレジスタスタック初期化
         initMemoryRegister();
         $.ajax({
-            url: 'http://localhost:8080/GCASL',
+            url: 'https://fast-river-46694.herokuapp.com/GCASL',
             type: 'POST',
             dataType: 'json',
             // フォーム要素の内容をハッシュ形式に変換
